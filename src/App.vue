3d0f1f6434e863152b29897297b44e742fa129a2
MT <984194616@qq.com>
2018-07-31 17:44:25 +0800

    MT

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+  <app-header></app-header>
+  </div>
+</template>
+
+<script>
+
+import AppHeader from './components/pages/header/header'
+export default {
+  name: 'App',
+  components: {
+      AppHeader : AppHeader
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/pages/header/header.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="app-header">
+        8888
+    </div>
+</template>  
+
+<script>
+    export default {
+        name:'AppHeader'
+    }
+
+</script> 
+
+<style >
+    #app-header{width:100px;height:200px;background:red;}
+
+
+</style>
